ac2ce1685c1caa1b10b3736d793b4d25ee0afcee
Dgeko <dfhdfh13@gmail.com>
2023-04-28 00:59:03 +0300

    в процессе настройка взаимодействия с films

---
+++ src/actors/dto/create-actor.dto.ts
@@ -0,0 +1,15 @@
+export class CreateActorDto{
+    readonly id: number;
+    readonly name: string;
+    readonly nameEn: string;
+    readonly sex: string;
+    readonly posterUrl: string;
+    readonly growth: string;
+    readonly birthday: string;
+    readonly death: string;
+    readonly age: string;
+    readonly birthplace: string;
+    readonly deathplace: string;
+    readonly profession: string;
+    readonly facts: string;
+}
