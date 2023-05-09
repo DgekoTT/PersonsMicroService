d2e7ec13e594ded98b48b349c22b49b3ea0c5c0b
Dgeko <dfhdfh13@gmail.com>
2023-05-09 11:26:19 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {CorsOptions} from "@nestjs/common/interfaces/external/cors-options.interface";
+import * as cors from 'cors';
 
 async function persons() {
   const PORT = process.env.PORT || 4070;
   const app = await NestFactory.create(AppModule);
+  // Использовать * для разрешения любого источника npm install --save @nestjs/platform-express cors
+  const corsOptions: CorsOptions = {
+    origin: '*',
+  };
+  app.use(cors(corsOptions));
   await app.listen(PORT, ()=> console.log(`Server Persons is started on PORT = ${PORT} `));
 }
 
