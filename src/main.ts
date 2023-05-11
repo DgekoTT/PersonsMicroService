7edce1b36ff5b40474682f2d31b9b02ae89ccd67
Dgeko <dfhdfh13@gmail.com>
2023-05-11 14:05:52 +0300

    fix fetch

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {CorsOptions} from "@nestjs/common/interfaces/external/cors-options.interface";
-import * as cors from 'cors';
+
 
 async function persons() {
   const PORT = process.env.PORT || 4070;
