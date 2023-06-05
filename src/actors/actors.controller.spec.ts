a6998d786f8de46e7cf56f797e36b14730a0e60d
DgekoTT <106529200+DgekoTT@users.noreply.github.com>
2023-06-05 13:06:39 +0300

    add tests

--- src/actors/actors.controller.spec.ts
+++ src/actors/actors.controller.spec.ts
@@ -1,18 +1,69 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ActorsController } from './actors.controller';
+import { ActorsService } from './actors.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('ActorsController', () => {
-  let controller: ActorsController;
+  let actorsController: ActorsController;
+  let spyService: ActorsService;
+
+  const mockActorsService = {
+    getActorByPersonId: jest.fn(() => {
+      return  {
+        personId: 999,
+        name: "Иван",
+        nameEn: "Ivan",
+        sex: "Male",
+        posterUrl: "none",
+        growth: "growth",
+        birthday: "09-09-1999",
+        death: "none",
+        age: "23",
+        birthplace: "birthplace",
+        deathplace: "deathplace",
+        profession: "profession",
+        facts: "facts"
+      }
+    })
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ActorsController],
-    }).compile();
+      providers: [
+        ActorsService,
+        JwtService
+      ]
+    }).overrideProvider(ActorsService)
+        .useValue(mockActorsService)
+        .compile();
 
-    controller = module.get<ActorsController>(ActorsController);
+    actorsController = module.get<ActorsController>(ActorsController);
+    spyService = module.get<ActorsService>(ActorsService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(actorsController).toBeDefined();
+  });
+
+  it('should be call loadActors', () => {
+    actorsController.getActorByPersonId(999)
+    expect(actorsController.getActorByPersonId(999)).toEqual(
+        {
+          personId: 999,
+          name: "Иван",
+          nameEn: "Ivan",
+          sex: "Male",
+          posterUrl: "none",
+          growth: "growth",
+          birthday: "09-09-1999",
+          death: "none",
+          age: "23",
+          birthplace: "birthplace",
+          deathplace: "deathplace",
+          profession: "profession",
+          facts: "facts"
+        }
+    );
   });
 });
