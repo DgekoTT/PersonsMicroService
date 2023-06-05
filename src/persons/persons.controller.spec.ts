bff6d063a1e52e2d7c3d200099dbc92862703c04
DgekoTT <106529200+DgekoTT@users.noreply.github.com>
2023-06-05 14:07:27 +0300

    add tests

--- src/persons/persons.controller.spec.ts
+++ src/persons/persons.controller.spec.ts
@@ -1,18 +1,103 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PersonsController } from './persons.controller';
+import { PersonsService } from './persons.service';
 
 describe('PersonsController', () => {
-  let controller: PersonsController;
+  let personsController: PersonsController;
+  let spyService: PersonsService;
+
+  const mockPersonsService = {
+    createPersons: jest.fn((dto) => {
+          return {
+            filmId: dto.filmId,
+            director: dto.director,
+            scenario: dto.scenario,
+            producer: dto.producer,
+            operator: dto.operator,
+            composer: dto.composer,
+            painter: dto.painter,
+            installation: dto.installation,
+            actors: dto.actors
+          }
+        }
+    ),
+
+    getPersonsByFilmId: jest.fn(() => {
+      return ["Дэвид Лодж",
+        "Николас Ройе",
+        "Джессика Ги",
+        "Бен Дискин",
+        "Грант Джордж",
+        "Фарук Тохид"]
+    })
+
+  }
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PersonsController],
-    }).compile();
+      providers: [
+        PersonsService,
 
-    controller = module.get<PersonsController>(PersonsController);
+      ]
+    })
+        .overrideProvider(PersonsService)
+        .useValue(mockPersonsService)
+        .compile();
+
+    personsController = module.get<PersonsController>(PersonsController);
+    spyService = module.get<PersonsService>(PersonsService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(personsController).toBeDefined();
+  });
+
+  it('should be called createPerson', () => {
+    const dto = {
+      filmId: 123,
+      director: "Дэвид Лодж",
+      scenario: "Николас Ройе",
+      producer: "Джессика Ги",
+      operator: "Бен Дискин",
+      composer: "Грант Джордж",
+      painter: "Фарук Тохид",
+      installation: "Фарук Тохид",
+      actors: ["Дэвид Лодж",
+        "Николас Ройе",
+        "Джессика Ги",
+        "Бен Дискин",
+        "Грант Джордж",
+        "Фарук Тохид"]
+    }
+    const dtoJson = JSON.stringify(dto);
+    personsController.createPersons(dtoJson);
+    expect(spyService.createPersons(dto)).toEqual({
+      filmId: 123,
+      director: "Дэвид Лодж",
+      scenario: "Николас Ройе",
+      producer: "Джессика Ги",
+      operator: "Бен Дискин",
+      composer: "Грант Джордж",
+      painter: "Фарук Тохид",
+      installation: "Фарук Тохид",
+      actors: ["Дэвид Лодж",
+        "Николас Ройе",
+        "Джессика Ги",
+        "Бен Дискин",
+        "Грант Джордж",
+        "Фарук Тохид"]
+    })
+  });
+
+  it('should be called getPerson', () => {
+    personsController.getPersons('123');
+    expect(spyService.getPersonsByFilmId(123)).toEqual(["Дэвид Лодж",
+      "Николас Ройе",
+      "Джессика Ги",
+      "Бен Дискин",
+      "Грант Джордж",
+      "Фарук Тохид"])
   });
 });
